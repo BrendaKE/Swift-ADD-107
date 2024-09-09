a8780ff87ca22ad99a8bf71425e3ad3ab1b80cc1
BrendaKE <beubanks@students.mchenry.edu>
2024-09-08 21:57:06 -0500

    Design First App Assignment

---
+++ DesignFirstAppBrendaE.playground/Contents.swift
@@ -0,0 +1,84 @@
+import UIKit
+
+print("Painting's by Brenda E. Cost Information:")
+print("")
+
+let artSuppliesPrice = 20
+let smallCanvasPrice = 25
+let mediumCanvasPrice = 50
+let largeCanvasPrice = 75
+
+var paintingLaborPrice = 0
+var myCanvasCost = 0
+
+
+//Large Canvas price
+paintingLaborPrice = 100
+artSuppliesPrice + largeCanvasPrice + paintingLaborPrice
+myCanvasCost = 195
+print("LARGE PAINTING")
+print("The price for a Large Canvas is $\(largeCanvasPrice).")
+print("The price for the painting supplies is $\(artSuppliesPrice).")
+print("The price for my labor painting the piece is $\(paintingLaborPrice).")
+print("The total price for a Large Canvas painting is $\(myCanvasCost).")
+myCanvasCost = 0
+
+//Medium Canvas price
+paintingLaborPrice = 80
+artSuppliesPrice + mediumCanvasPrice + paintingLaborPrice
+myCanvasCost = 150
+print("")
+print("MEDIUM PAINTING")
+print("The price for a Medium Canvas is $\(mediumCanvasPrice).")
+print("The price for the painting supplies is $\(artSuppliesPrice).")
+print("The price for my labor painting the piece is $\(paintingLaborPrice).")
+print("The total price for a Medium Canvas painting is $\(myCanvasCost).")
+myCanvasCost = 0
+
+//Small Canvas price
+paintingLaborPrice = 50
+artSuppliesPrice + smallCanvasPrice + paintingLaborPrice
+myCanvasCost = 95
+print("")
+print("SMALL PAINTING")
+print("The price for a Small Canvas is $\(smallCanvasPrice).")
+print("The price for the painting supplies is $\(artSuppliesPrice).")
+print("The price for my labor painting the piece is $\(paintingLaborPrice).")
+print("The total price for a Small Canvas painting is $\(myCanvasCost).")
+myCanvasCost = 0
+
+print("")
+print("Thank you for your interest in my paintings!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
