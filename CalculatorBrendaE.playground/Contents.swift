df6f2e5eaa9ace17b704abc4d310fbc0671e1004
BrendaKE <beubanks@students.mchenry.edu>
2024-09-16 19:18:20 -0500

    Calculator Assignment

---
+++ CalculatorBrendaE.playground/Contents.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+var additionFirstNumber = 0
+var additionSecondNumber = 0
+var subtractionFirstNumber = 0
+var subtractionSecondNumber = 0
+var multiplyFirstNumber = 0
+var multiplySecondNumber = 0
+var divideFirstNumber = 0
+var divideSecondNumber = 0
+var percentageFirstNumber = 0.0
+var percentageSecondNumber = 0.0
+
+//Addition
+additionFirstNumber = 8
+additionSecondNumber = 14
+var additionTotal = additionFirstNumber + additionSecondNumber
+
+//Subtraction
+subtractionFirstNumber = 45
+subtractionSecondNumber = 17
+var subtractionTotal = subtractionFirstNumber - subtractionSecondNumber
+
+//Multiply
+multiplyFirstNumber = 4
+multiplySecondNumber = 11
+var multiplicationTotal = multiplyFirstNumber * multiplySecondNumber
+
+//Divide
+divideFirstNumber = 500
+divideSecondNumber = 25
+var divideTotal = divideFirstNumber / divideSecondNumber
+
+//Percentages
+percentageFirstNumber = 8
+percentageSecondNumber = 4.5
+var percentageResult = percentageSecondNumber / percentageFirstNumber * 100
+
+
+print("Welcome to the Calculator App!")
+print("")
+
+print("\(additionFirstNumber) + \(additionSecondNumber) = \(additionTotal)")
+print("\(subtractionFirstNumber) - \(subtractionSecondNumber) = \(subtractionTotal)")
+print("\(multiplyFirstNumber) * \(multiplySecondNumber) = \(multiplicationTotal)")
+print("\(divideFirstNumber) / \(divideSecondNumber) = \(divideTotal)")
+print("\(percentageSecondNumber) is \(percentageResult)% of \(percentageFirstNumber)")
+
+print("")
+print("This concludes your calculation results.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
