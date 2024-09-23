04f3129a8eaa7a9b18687c592d5be9521e1fe6c5
BrendaKE <beubanks@students.mchenry.edu>
2024-09-22 23:18:40 -0500

    Clock Recipe Assignment

---
+++ ClockRecipeBrendaE.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+// Clock Recipe Assignment
+
+// Ingredients for a Clock
+// second
+// minute
+// hour
+
+
+// Directions
+// Add 60 seconds to make a minute
+// Add 60 minutes to make an hour
+// Add hours until 24 is reached
+// After 24 hours, reset to 0
+
+
+
+
+
+
+
